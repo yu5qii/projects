dcb7447f0b7763329c00c2b96d272d1128a48fca
Ridah <ridah.25b01010047@abes.ac.in>
2025-12-04 16:24:10 +0530

    abstraction

---
+++ SELF-STUDY/ABSTRACTION IN C++.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+//ABSTRACTION: displaying only essential information about the data and ignoring the intername details 
+//TYPES OF ABSTRACTION:
+//1. Bata abstraction: only shows required information about the data
+//2. Control abstraction: only shows the required info~ about the implimentation 
+//USING ACCESS SPECIFIERS: used to enforce restrictions on class memebers
+//public members can be access from anywhere in the program, private members can only be access from within the class
+
+//ABSTRACTION USING ABSTRACT KEYWORD (using pure virtual functions)
+
+class impabs
+{
+    private:
+        int a, b;
+
+    public:
+        void set (int x, int y)
+        {
+            a = x;
+            b = y;
+        }
+
+        void display()
+        {
+            cout << a << endl;
+            cout << b << endl;
+        }
+};
+
+int main()
+{
+    impabs object;
+    int a, b;
+    cin >> a >> b;
+    object.set(a, b);
+    object.display();
+
+    return 0;
+}
